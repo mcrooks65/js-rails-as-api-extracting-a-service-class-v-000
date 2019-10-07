2acb801425ad4ec04b525417623a94e59e64183c
Matthew Crooks <mfcrooks@gmail.com>
2019-10-07 23:56:04 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -1,2 +1,11 @@
-class SightingSerializer
+class SightingsController < ApplicationController
+  def index
+    sightings = Sighting.all
+    render json: SightingSerializer.new(sightings).to_serialized_json
+  end
+ 
+  def show
+    sighting = Sighting.find_by(id: params[:id])
+    render json: SightingSerializer.new(sighting).to_serialized_json
+  end
 end

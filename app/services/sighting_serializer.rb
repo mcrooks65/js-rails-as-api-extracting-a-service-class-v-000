6d641991bbb146e12cc2a46df931ea42753dc89c
Matthew Crooks <mfcrooks@gmail.com>
2019-10-07 23:57:21 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -9,3 +9,18 @@ class SightingsController < ApplicationController
     render json: SightingSerializer.new(sighting).to_serialized_json
   end
 end
+
+def to_serialized_json
+  options = {
+    include: {
+      bird: {
+        only: [:name, :species]
+      },
+      location: {
+        only: [:latitude, :longitude]
+      }
+    },
+    except: [:updated_at],
+  }
+  @sighting.to_json(options)
+end
